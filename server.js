07a485a53ffbe1c538f45743d12d3d23edca89bc
Joseph Rimar <josephrimar@yahoo.com>
2016-02-01 21:40:09 -0500

    db update

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ var app = express();
 var PORT = process.env.PORT || 5000;
 var mongoose = require('mongoose');
 
+//If hosted on heroku
 if(process.env.PORT) {
 	//connect to correct remote db
-	mongoose.connect('');
+	mongoose.connect('mongodb://joe:password@ds055545.mongolab.com:55545/joesfreecodecamp');
+//If on local machine
 } else {
 	mongoose.connect('mongodb://localhost/fccurls');
 }
